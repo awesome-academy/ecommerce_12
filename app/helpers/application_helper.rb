89e4759da06d7dcf4d7dd97ffe085736ef36d343
Tuan Nguyen <vantuantk4@gmail.com>
2019-08-30 11:20:59 +0700

    Design static pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def full_title page_title
-    base_title = I18n.t(".nameweb")
+    base_title = I18n.t(".page_title")
     if page_title.blank?
       base_title
     else
